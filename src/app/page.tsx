8c00711165ba776e11ca2622487624f6156a8032
Darshan Rajput <rajputdarshanashok@gmail.com>
2024-12-02 18:31:58 +0530

    Add Image store state using zustand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import Editor from "@/components/editor";
+import { ImageStore } from "@/lib/image-store";
 
 export default function Home() {
   return (
-    <main>
-      <Editor />
-    </main>
+    <ImageStore.Provider initialValue={{ generating: false }}>
+      <main>
+        <Editor />
+      </main>
+    </ImageStore.Provider>
   );
 }
