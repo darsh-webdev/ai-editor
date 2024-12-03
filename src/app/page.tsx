2a04addc309927db8a87919dd6e7205bc0702e5d
Darshan Rajput <rajputdarshanashok@gmail.com>
2024-12-03 22:42:54 +0530

    Add Layers store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
 "use client";
 import Editor from "@/components/editor";
 import { ImageStore } from "@/lib/image-store";
+import { LayerStore } from "@/lib/layer-store";
 
 export default function Home() {
   return (
-    <ImageStore.Provider initialValue={{ generating: false }}>
-      <main>
-        <Editor />
-      </main>
-    </ImageStore.Provider>
+    <LayerStore.Provider
+      initialValue={{
+        layers: [
+          {
+            id: crypto.randomUUID(),
+            url: "",
+            height: 0,
+            width: 0,
+            publicId: "",
+          },
+        ],
+        layerComparisonMode: false,
+      }}
+    >
+      <ImageStore.Provider initialValue={{ generating: false }}>
+        <main>
+          <Editor />
+        </main>
+      </ImageStore.Provider>
+    </LayerStore.Provider>
   );
 }
